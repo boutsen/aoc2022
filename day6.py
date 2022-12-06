004c871ac217b0e782c0ae40c85d5cf3ae23c637
Kevin Boutsen <kevin.boutsen@ctg.com>
2022-12-06 09:20:09 +0100

    Simplified as the function set already removes duplicates

--- day6.py
+++ day6.py
@@ -2,21 +2,10 @@ f = open("inputs/day6", "r")
 
 DATA = f.read()
 
-def check_unique(str):
-    for i in range(len(str)):
-        for j in range(i + 1,len(str)):
-            if(str[i] == str[j]):
-                return False
-    return True
+def tuning2(data,filter):
+    for i in range(0,len(data)-(filter-1)):
+        if( len(set(data[i:i+filter])) == filter):
+            return i+filter
 
-
-def tuning(data,filter):
-    for set in [data[i:i+filter] for i in range(0,len(DATA)-(filter-1))]:
-        if check_unique(set):
-            return data.index(set)+filter
-
-
-print(tuning(DATA,4))
-print(tuning(DATA,14))
-
-
+print(tuning2(DATA,4))
+print(tuning2(DATA,14))
