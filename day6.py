5596e9ebc35793cea99d7747a5b41a581efbf80e
Kevin Boutsen <kevin.boutsen@ctg.com>
2022-12-06 09:20:48 +0100

    Simplified as the function set already removes duplicates

--- day6.py
+++ day6.py
@@ -2,10 +2,10 @@ f = open("inputs/day6", "r")
 
 DATA = f.read()
 
-def tuning2(data,filter):
+def tuning(data,filter):
     for i in range(0,len(data)-(filter-1)):
         if( len(set(data[i:i+filter])) == filter):
             return i+filter
 
-print(tuning2(DATA,4))
-print(tuning2(DATA,14))
+print(tuning(DATA,4))
+print(tuning(DATA,14))
