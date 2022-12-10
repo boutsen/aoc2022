c3620bccf354c4b8a5cc612b407d2b142b29393d
Kevin Boutsen <boutsen@gmail.com>
2022-12-10 11:55:10 +0100

    Day 10

---
+++ day10.py
@@ -0,0 +1,22 @@
+f = open("inputs/day10", "r")
+
+OPERATIONS, LIGHT, DARK = [], "##", ".."
+
+for line in f.readlines():
+    OPERATIONS.append(0)
+    if "addx" in line:
+        OPERATIONS.append(int(line.split(" ")[1]))
+
+print("Part1:", sum([(1 + sum([OPERATIONS[cycle] for cycle in range(n-1)]))*n for n in [20, 60, 100, 140, 180, 220]]))
+
+register, CRT = 1, ""
+for cycle, operation in enumerate(OPERATIONS):
+    if cycle % 40 == 0:
+        CRT += "\n"
+    if abs((cycle % 40)-register) < 2:
+        CRT += LIGHT
+    else:
+        CRT += DARK
+    register += operation
+
+print("Part2:", "".join(CRT))
