0c31d1a44e74bb7bf323f20b09305966a193ce5e
Kevin Boutsen <boutsen@gmail.com>
2022-12-13 13:28:04 +0100

    Day 13

---
+++ day13.py
@@ -0,0 +1,39 @@
+f = open("inputs/day13", "r")
+
+DATA = [tuple(map(eval, line.split("\n"))) for line in f.read().split("\n\n")]
+
+
+def compare(left, right):
+    n1, n2 = len(left), len(right)
+
+    for i in range(max(n1, n2)):
+        if i >= n1: return True
+        if i >= n2: return False
+
+        (l, r) = (left[i], right[i])
+        if type(l) is int and type(r) is int:
+            if l < r: return True
+            elif l > r: return False
+        else:
+            if type(l) is int: l = [l]
+            if type(r) is int: r = [r]
+
+            res = compare(l, r)
+            if res is not None: return res
+
+
+print(sum([(i+1) for i, (l, r) in enumerate(DATA) if compare(l, r)]))
+
+
+def sort(l):
+    n = len(l)
+    for i in range(n - 1):
+        for j in range(0, n - i - 1):
+            if compare(l[j + 1], l[j]):
+                l[j], l[j + 1] = l[j + 1], l[j]
+
+
+L1, L2 = zip(*DATA)
+PART2 = list(L1+L2+([[2]], [[6]]))
+sort(PART2)
+print((PART2.index([[2]])+1)*(PART2.index([[6]])+1))
